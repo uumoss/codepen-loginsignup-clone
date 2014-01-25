fab7d40678820b98d571d287773cb9212bf9630b
Wesley <uumoss@gmail.com>
2014-01-25 13:20:03 -0700

    changed javascript

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ codepen.api.signup(user_object)
 	parameters: user object, which contains properties: name, email, username, password
 	returns: response object
 
-codepen.login(user_object)
+codepen.api.login(user_object)
 	description: to login an existing user
 	parameters: user object, which contains properties: username, password
 	returns: response object
@@ -31,6 +31,57 @@ Reponse Objects:
 
 $('document').ready(function() {
 
-	//put code here
+	codepen.objects.User = {
+		name: null,
+		email: null,
+		username: null,
+		password: null,
+	}
+
+	var NewUser = Object.create(codepne.objects.User, {
+		firstname: {
+			writable: true,
+			enumerable: true,
+			value: ''
+		}
+		lastname: {
+			writable: true,
+			enumerable: true,
+			value: ''
+		}
+	});
+
+	
+	$('.signup-form').hide();
+	$('.signup-form-btn').click(function(){
+		$('.login-form').hide();
+		$('.signup-form').show();
+		$('.login-form-btn').removeClass('active');
+		$('.signup-form-btn').addClass('active');
+	})
+	$('.login-form-btn').click(function(){
+		$('.login-form').show();
+		$('.signup-form').hide();
+		$('.login-form-btn').addClass('active');
+		$('.signup-form-btn').removeClass('active');
+	});
+
+	$('.btn-login').click(function(){
+		var loginUser = {};
+			loginUser.username = $('#login-username-field').val();
+			loginUser.password = $('#login-password-field').val();
+			console.log((codepen.api.login(loginUser)));
+	});
+
+	$('.btn-signup').click(function(){
+		var signupUser = {};
+			signupUser.name = $('#signup-name-field').val();
+			signupUser.email = $('#signup-email-field').val();
+			signupUser.username = $('#signup-username-field').val();
+			signupUser.password = $('#signup-password-field').val();
+			signupUser.password = $('#signup-password-field').val();
+			console.log((codepen.api.signup(signupUser)));
+	});
+
 
 });
